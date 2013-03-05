e20ecb578c4a8c9a85e78838d3d0bba7d629952b
claudiosmweb <contato@claudiosmweb.com>
2013-03-05 02:03:53 -0300

    generated mixins.scss compilator

--- compass.py
+++ compass.py
@@ -17,12 +17,7 @@ def generate_woocommerce_base_scss():
     old.close()
 
     lines = {
-        # """ """: """ """,
-        """@primary""": """$primary""",
-        """@secondary""": """$secondary""",
-        """@highlight""": """$highlight""",
-        """@contentbg""": """$contentbg""",
-        """@subtext""": """$subtext""",
+        """@""": """$""",
         """spin(""": """adjust-hue(""",
     }
 
@@ -32,7 +27,45 @@ def generate_woocommerce_base_scss():
     new.write(result)
     new.close()
 
+def generate_mixins_scss():
+    base_file = './less/mixins.less'
+    base_output = './sass/_mixins.scss'
+
+    old = file(base_file, 'r')
+    content = old.read()
+    old.close()
+
+    lines = {
+        # """ """: """ """,
+        """@""": """$""",
+        """.clearfix()""": """@mixin clearfix()""",
+        """.border_radius""": """@mixin border_radius""",
+        """.opacity(""": """@mixin opacity(""",
+        """.box_shadow(""": """@mixin box_shadow(""",
+        """.inset_box_shadow(""": """@mixin inset_box_shadow(""",
+        """.text_shadow(""": """@mixin text_shadow(""",
+        """.text_shadow(""": """@mixin text_shadow(""",
+        """.vertical_gradient(""": """@mixin vertical_gradient(""",
+        """.transition(""": """@mixin transition(""",
+        """.scale(""": """@mixin scale(""",
+        """.borderbox ()""": """@mixin borderbox()""",
+        """.darkorlighttextshadow (""": """@mixin darkorlighttextshadow(""",
+
+        # fix @mixins
+        """$mixin""": """@mixin""",
+        """{ @mixin text_shadow(""": """{ @include text_shadow(""",
+        """filter:~\"alpha(opacity=$opacity * 100)\";""": """filter: alpha(opacity=( $opacity * 100 ) );""",
+        """ when """: """ { @if """,
+        """$opacity) ); }""": """$opacity) ); } }""",
+    }
+
+    result = replace_all(content, lines)
+
+    new = file(base_output, 'w')
+    new.write(result)
+    new.close()
 
 generate_woocommerce_base_scss()
+generate_mixins_scss()
 
 print 'Compiled!'
